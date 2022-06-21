dba7f69e9b8f0f78b7af0568a1a67e0352040047
Ola Tomaszewska <aleksandra_tomaszewska@onet.eu>
2022-06-21 10:34:36 +0200

    Developing docker-compose file, work on tests to wait for db file

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""Calculator functions"""
+
+
+def add(x, y):
+    """Add x and y and return result"""
+    return x + y
+
+
+def substruct(x, y):
+    """substruct x and y and give the result"""
+    return x - y
